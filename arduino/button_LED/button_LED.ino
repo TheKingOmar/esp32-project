488dc6596dfc9473234f76c8387b455c3cdcd6d2
Omar <wlamasat@gmail.com>
2026-04-12 22:02:18 +0200

    Add millis-based button toggle with independent status timer

--- arduino/button_LED/button_LED.ino
+++ arduino/button_LED/button_LED.ino
@@ -1,20 +1,47 @@
 const int buttonPin = 4;
 const int ledPin = 2;
-
+const unsigned long intervalButton = 30;
+const unsigned long intervalStatusPrint = 500;
+unsigned long lastTimeButton = 0;
+unsigned long lastTimeStatusPrint = 0;
+int lastButtonStatus = HIGH;
+bool ledState = false;
 void setup() {
   Serial.begin(115200);
   pinMode(buttonPin, INPUT_PULLUP);
   pinMode(ledPin, OUTPUT);
+  digitalWrite(ledPin, LOW);
 }
 
 void loop() {
+  unsigned long currentTime = millis();
+  if (currentTime - lastTimeButton >= intervalButton){
   int buttonStatus = digitalRead(buttonPin);
-  if (buttonStatus == LOW){
-    Serial.println("Pressed");
+  if (buttonStatus != lastButtonStatus){
+    if (lastButtonStatus == HIGH && buttonStatus == LOW){
+    ledState = !ledState;
+    if (ledState){
     digitalWrite(ledPin, HIGH);
-  }else {
-    Serial.println("released");
+  } else{
     digitalWrite(ledPin, LOW);
   }
-  delay(100);
+    if (ledState){
+      Serial.println("LED turned ON");
+    } else {
+      Serial.println("LED turned OFF");
+    }
+    }
+    lastButtonStatus = buttonStatus;
+  }
+  lastTimeButton = currentTime;
+  }
+  if(currentTime - lastTimeStatusPrint >= intervalStatusPrint){
+    if (ledState){
+      Serial.println("LED is ON");
+    } else {
+      Serial.println("LED is OFF");
+    }
+    lastTimeStatusPrint = currentTime;
+  }
+
 }
